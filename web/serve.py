1660dcb63d516a93457f8fa49d4009aa45b45df6
Seth Troisi <sethtroisi@google.com>
2018-05-12 01:02:57 -0700

    Fix winrate calculation

--- web/serve.py
+++ web/serve.py
@@ -533,7 +533,8 @@ def model_eval(bucket, model_name):
         # Make models more familiar
         cur_id = e_m[0] % CloudyGo.SALT_MULT
         other_id = e_m[1] % CloudyGo.SALT_MULT
-        winrate = 100 / (1 + 10 ** ((e_m[2] - rating) / 400))
+        rating_diff = 2 * (e_m[2] - rating)
+        winrate = 100 / (1 + 10 ** (rating_diff / 400))
 
         updated.append((cur_id, other_id, winrate) + e_m[3:])
 
