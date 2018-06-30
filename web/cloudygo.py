ab7544c9ae99285c5bac82d5d6375d65ba782061
Seth Troisi <sethtroisi@google.com>
2018-06-30 15:51:02 -0700

    fix bug in eval model id

--- web/cloudygo.py
+++ web/cloudygo.py
@@ -828,7 +828,7 @@ class CloudyGo:
                 return name_to_num[name]
 
             if re.match(r'[0-9]{6}-([a-zA-Z-]+)', name):
-                return int(name.split('-', 1)[0])
+                return bucket_salt + int(name.split('-', 1)[0])
 
             keys = set(name_to_num.values())
             first_eval_model = bucket_salt + CloudyGo.DIR_EVAL_START
