78264cd0e76b7db90d44a654a76345026201bcc9
Seth Troisi <sethtroisi@google.com>
2018-10-07 20:23:38 -0700

    Uniform sampling of full directories

--- rsync-data.sh
+++ rsync-data.sh
@@ -101,7 +101,7 @@ esac
 
 # gsutil doesn't like // in folder paths which happens if RUN is empty.
 base_cloud_path="gs://$(echo "$BUCKET/$RUN/" | sed 's#//#/#g')"
-if [[ ! -z "$MODELS" ]]; then
+if [[ "$MODELS" ]]; then
     cloud_model_path="${base_cloud_path}models/"
     echo "Syncing model files from $cloud_path, at $(date)"
 
@@ -110,7 +110,7 @@ if [[ ! -z "$MODELS" ]]; then
     exit 0;
 fi
 
-if [[ ! -z "$EVALS" ]]; then
+if [[ "$EVALS" ]]; then
     cloud_path="${base_cloud_path}sgf/eval"
     echo "Syncing eval files from $cloud_path, at $(date)"
 
@@ -134,17 +134,17 @@ fi
 cloud_path="${base_cloud_path}sgf"
 partial_dest="$DEST/$RUN/sgf"
 echo "Getting models list from $cloud_path"
-if [[ -z "$TIME_BASED_NAMES" ]]; then
-    models=$(gsutil ls "$cloud_path" | grep -o "00[0-9]\{4\}-[a-z-]*/")
-else
+if [[ "$TIME_BASED_NAMES" ]]; then
     models=$(gsutil ls "$cloud_path/clean" | grep -o "2018-[0-9-]\{8\}/")
+else
+    models=$(gsutil ls "$cloud_path" | grep -o "00[0-9]\{4\}-[a-z-]*/")
 fi
 
 first_model=$(echo "$models" | head -n 1)
 last_model=$(echo "$models" | tail -n 1)
 echo "Found $(echo "$models" | wc -l) models ($first_model to $last_model)"
 
-if [[ ! -z "$MIN_MODEL" ]]; then
+if [[ "$MIN_MODEL" ]]; then
     models=$(echo "$models" | sed 's#^\(\([0-9-]*\)\(-.*\|/\)\)$#\2 \1#' |
                     awk -v min="$MIN_MODEL" '$1 >= min { print $2 }')
     echo "Syncing $(echo "$models" | wc -l) models >= $MIN_MODEL"
@@ -167,7 +167,8 @@ function gs_rsync() {
     echo -e "\t\e[1;32m$1 => $2\e[0m"
     echo -e "\t$(date)"
 
-    if [[ ! -z "$MAX_COUNT" ]]; then
+    if [[ "$MAX_COUNT" ]]; then
+
         existing="$(ls "$2" | wc -l)"
         echo -e "\texisting: $existing"
         if [[ "$existing" -ge "$MAX_COUNT" ]]; then
@@ -191,18 +192,27 @@ do
     echo
     echo -e "\e[1;32mSyncing $model $(date)\e[0m"
     if [[ -z "$DRY_RUN" ]]; then
-        if [[ -z "$TIME_BASED_NAMES" ]]; then
-            full_path="$model/full"
-            clean_path="$model/clean"
-        else
+        if [[ "$TIME_BASED_NAMES" ]]; then
+            # NOTE: Wait one hour To ensure uniform sample of TIME_BASED_NAMES
+            last_model=$(echo "$models" | tail -n 1)
+            if [[ "$MAX_COUNT" ]] && [[ "$last_model" == "$model" ]]; then
+                echo -e "\tSkipping recent model: $model"
+                continue
+            fi
+
             full_path="full/$model"
             clean_path="clean/$model"
+        else
+            full_path="$model/full"
+            clean_path="$model/clean"
         fi
 
-        if [[ ! -z "$CLEAN" ]]; then
+        if [[ "$CLEAN" ]]; then
             gs_rsync "$cloud_path/$clean_path" "$partial_dest/$clean_path"
         fi
-        if [[ ! -z "$FULL" ]]; then
+        if [[ "$FULL" ]]; then
+            # TODO syncing this before the end of the hour leads to problems :/
+            # especially given how cron runs right at the start of the hour.
             gs_rsync "$cloud_path/$full_path" "$partial_dest/$full_path"
         fi
     fi
