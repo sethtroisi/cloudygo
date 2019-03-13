306843ad4cd2c0413704cca1bc7b1eca17f1e2d7
Seth Troisi <sethtroisi@google.com>
2019-03-12 21:41:27 -0700

    Added sgfstrip script

--- oneoff/sgfstrip-eval.sh
+++ oneoff/sgfstrip-eval.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -euo pipefail
+
+# find 000* -type f -path '*/clean/*sgf' | sort | tqdm | tar cjf 'clean.tar.bz2' -T -
+
 # Arg checking
 if [[ "$#" -ne 1 ]]; then
     echo "Need run arg"
@@ -26,12 +30,26 @@ if [[ "$PWD" = *oneoff* ]]; then
 fi
 
 run=$1
-echo "Setting up run: \"$run\""
+echo "SGFStrip eval run: \"$run\""
 
-mkdir -p "instance/data/$run"
-mkdir -p "instance/data/$run/models"
-mkdir -p "instance/data/$run/sgf"
-mkdir -p "instance/data/$run/eval"
-mkdir -p "instance/eval/$run"
-mkdir -p "instance/policy/$run"
-mkdir -p "instance/pv/$run"
+if [[ ! -d "instance/data/$run/eval/" ]]; then
+    echo "No eval for $run"
+    exit 2;
+fi
+
+#if [[ -d "instance/data/$run/eval_clean/" ]]; then
+#    echo "eval clean already exists for $run"
+#    exit 2;
+#fi
+
+cd "instance/data/$run"
+
+echo "Recreate directory structure"
+time find -L eval -type d -printf "%P\0" | xargs -0 -I{} mkdir -p eval_clean/{}
+
+echo "Running SGFstrip"
+total_sgfs=$(find -L eval -type f -iname '*.sgf' | wc -l)
+time find -L eval -type f -iname '*.sgf' -printf "%P\0" \
+    | tqdm --delim '\0' --unit "SGF" --total $total_sgfs \
+    | xargs -0 -I{} sh -c 'sgfstrip -h C < "eval/{}" > "eval_clean/{}"'
+
